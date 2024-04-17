3b1db744ad038e4e08f6636d89179c992b1fae5c
Prajwal Sonawane <89967411+Prajwalsonawane5991@users.noreply.github.com>
2024-04-17 18:23:22 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import FilterData from './Component/FilterData';
+import { Footer, Header } from './Component/Headercomponent';
+//import AboutUsPage from './Component/About';
+
+import SummaryTable from './Component/SummaryTable';
+
+
+function App() {
+  return (
+          <div>
+   
+
+   <Header></Header>
+   <FilterData></FilterData>
+   <Footer></Footer>
+    </div>
+  
+  );
+}
+
+export default App;
